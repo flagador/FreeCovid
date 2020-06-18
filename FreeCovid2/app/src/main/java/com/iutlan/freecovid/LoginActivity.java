5d0c0dddd1324702b7f5f960622575a2a91be8c7
Julian LEBOUC <julian.lebouc@etudiant.univ-rennes1.fr>
2020-06-18 14:17:30 +0200

    Methode OnClick dans LoginActivity

--- FreeCovid2/app/src/main/java/com/iutlan/freecovid/LoginActivity.java
+++ FreeCovid2/app/src/main/java/com/iutlan/freecovid/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.iutlan.freecovid;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,24 @@ public class LoginActivity extends AppCompatActivity {
         // mettre en place l'interface
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        Button button = (Button) findViewById(R.id.button);
+        final EditText editEmail, editMotdepasse;
+        editEmail = (EditText) findViewById(R.id.email);
+        editMotdepasse = (EditText) findViewById(R.id.motdepasse);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editEmail.getText().toString();
+                String motdepasse = editMotdepasse.getText().toString();
+                ConnexionBdd con = new ConnexionBdd();
+                if(con.verifConnexion(email,motdepasse)==true){
+                    // UTILISER LA METHODE UPDATEBDD POUR ENVOYER LA LISTE D'ID ENVOYES SUR LA BDD
+                } else {
+                    System.out.println("Email ou mot de passe incorrect");
+                }
+
+            }
+        });
 
     }
 }
