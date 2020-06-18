a8cba72864957b0c6983ff823d10e42ff6e07f5a
Julian LEBOUC <julian.lebouc@etudiant.univ-rennes1.fr>
2020-06-18 14:53:17 +0200

    initialisation variables dans MainActivity

--- FreeCovid2/app/src/main/java/com/iutlan/freecovid/MainActivity.java
+++ FreeCovid2/app/src/main/java/com/iutlan/freecovid/MainActivity.java
@@ -9,10 +9,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    IdEnvoyes idenv = new IdEnvoyes();
+    IdRecus idrec = new IdRecus();
+    Bluetooth bt = new Bluetooth();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt.run();
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
