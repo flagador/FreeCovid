928bf77f227f9d641ea24940fc7658e4f8759475
Julian LEBOUC <julian.lebouc@etudiant.univ-rennes1.fr>
2020-06-22 23:52:17 +0200

    ..

--- FreeCovid2/app/src/main/java/com/iutlan/freecovid/MainActivity.java
+++ FreeCovid2/app/src/main/java/com/iutlan/freecovid/MainActivity.java
@@ -17,8 +17,7 @@ public class MainActivity extends AppCompatActivity {
     ListeID idrec = new IdRecus();
     IdentifiantSimple id = new IdentifiantSimple();
     Button button;
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
